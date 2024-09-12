ca5547153d1b018c348863e3b664742e8d40ec05
nicollsusano <i202214890@cibertec.edu.pe>
2024-09-11 22:11:17 -0500

    Mi primer Commit

---
+++ src/main/java/com/prestamo/Dawii202405Semana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawii202405Semana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawii202405Semana01Application.class, args);
+	}
+
+}
